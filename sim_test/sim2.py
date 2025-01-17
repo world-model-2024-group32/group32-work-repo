d15fc7c5b5d96a25b165f22b6f59a053671ec2ae
vamos23467 <aoiro23467@gmail.com>
2025-01-17 16:56:34 +0900

    add sim program

--- sim_test/sim2.py
+++ sim_test/sim2.py
@@ -1,138 +1,82 @@
+# 矢印キーでアームを操作するプログラム
 import pybullet as p
 import time
 import pybullet_data
 import numpy as np
-from scipy.optimize import minimize
 
-# PyBulletのGUIモードで接続
 p.connect(p.GUI)
 p.setGravity(0, 0, -9.81)
 p.setAdditionalSearchPath(pybullet_data.getDataPath())
 
-# ロボットのURDFファイルをロード (AL5D)
-urdf_file = "../al5d_description/urdf/al5d_robot.urdf"
-robot_id = p.loadURDF(urdf_file, basePosition=[0, 0, 0.1], useFixedBase=True)
+
+robot_orientation = p.getQuaternionFromEuler([0, 0, np.pi]) 
+robot_id = p.loadURDF("../al5d_description/urdf/al5d_robot.urdf", basePosition=[0.6, 0, 0.6], baseOrientation=robot_orientation, useFixedBase=True, globalScaling=2.0)
+
+
+plane_id = p.loadURDF("plane.urdf")
+table_id = p.loadURDF("table/table.urdf", basePosition=[0.5, 0, 0])
+
+object_id = p.loadURDF("random_urdfs/000/000.urdf", basePosition=[0.0, 0.2, 0.6], globalScaling=0.9)
 
 num_joints = p.getNumJoints(robot_id)
+end_effector_link_index = num_joints - 1
 
-# リンク長 (mm) - 実際にはURDF等から正確に取得すべき
-L1 = 145.0
-L2 = 130.0
-L3 = 60.0
-L4 = 100.0
-joint_lengths = [L1, L2, L3, L4]
+# カメラ位置を調整
+p.resetDebugVisualizerCamera(cameraDistance=1.5, cameraYaw=90, cameraPitch=-45, cameraTargetPosition=[0,0,1.2])
 
-# カメラ調整（近接させる）
-p.resetDebugVisualizerCamera(cameraDistance=0.5, cameraYaw=90, cameraPitch=-30, cameraTargetPosition=[0,0,0.1])
+joint_angles = [0] * num_joints
+joint_angles[2] -= 1.5
 
-def rotation_matrix(axis, angle):
-    """
-    軸(axis)周りにangle[rad]回転する回転行列を返す
-    axisは'x', 'y', 'z'のいずれか
-    """
-    c = np.cos(angle)
-    s = np.sin(angle)
-    if axis == 'x':
-        return np.array([[1, 0, 0],
-                         [0, c, -s],
-                         [0, s, c]])
-    elif axis == 'y':
-        return np.array([[c, 0, s],
-                         [0, 1, 0],
-                         [-s,0, c]])
-    elif axis == 'z':
-        return np.array([[c, -s, 0],
-                         [s,  c, 0],
-                         [0,  0, 1]])
-    else:
-        return np.eye(3)
+def update_joint_angles(key):
+    global joint_angles
+    if key == p.B3G_LEFT_ARROW:
+        joint_angles[0] -= 0.1
+    elif key == p.B3G_RIGHT_ARROW:
+        joint_angles[0] += 0.1
+    elif key == p.B3G_UP_ARROW:
+        joint_angles[1] += 0.1
+        joint_angles[2] -= 0.1
+        joint_angles[3] += 0.1 
+    elif key == p.B3G_DOWN_ARROW:
+        joint_angles[1] -= 0.1 
+        joint_angles[2] += 0.1 
+        joint_angles[3] -= 0.1
+    print(joint_angles)
 
-def forward_kinematics(angles, joint_lengths):
-    """
-    非常に簡略化したFK関数。  
-    仮定:  
-    J1: z軸回り回転  
-    J2: y軸回り回転  
-    J3: y軸回り回転  
-    J4: y軸回り回転  
+# urdfにエンドエフェクタのリンクが定義されていないため、動作しない
+def grasp_object():
+    end_effector_state = p.getLinkState(robot_id, end_effector_link_index)
+    end_effector_pos = end_effector_state[0]
 
-    各ジョイント回転後、x方向にリンクが伸びると仮定する。
-    """
-    # anglesはdegree
-    th1, th2, th3, th4 = np.radians(angles)
-    
-    # 初期姿勢
-    pos = np.array([0,0,0])
-    R = np.eye(3)
+    object_pos, object_orn = p.getBasePositionAndOrientation(object_id)
 
-    # J1 (Z軸回り)
-    R = R @ rotation_matrix('z', th1)
-    pos = pos + R @ np.array([0,0,0])  # ベースから始まるのでオフセットなし
-    # L1リンクはx方向に伸びると仮定
-    pos = pos + R @ np.array([joint_lengths[0],0,0])
+    distance = np.linalg.norm(np.array(end_effector_pos) - np.array(object_pos))
 
-    # J2 (Y軸回り)
-    R = R @ rotation_matrix('y', th2)
-    pos = pos + R @ np.array([joint_lengths[1],0,0])
-
-    # J3 (Y軸回り)
-    R = R @ rotation_matrix('y', th3)
-    pos = pos + R @ np.array([joint_lengths[2],0,0])
-
-    # J4 (Y軸回り)
-    R = R @ rotation_matrix('y', th4)
-    pos = pos + R @ np.array([joint_lengths[3],0,0])
-
-    return pos
-
-def objective(angles, target_position, joint_lengths):
-    # FK計算
-    end_effector_pos = forward_kinematics(angles, joint_lengths)
-    return np.sum((end_effector_pos - target_position)**2)
-
-def get_ray_from_screen(x, y, view_matrix, projection_matrix):
-    # スクリーン座標を正規化デバイス座標に変換
-    screen_width = 1024
-    screen_height = 768
-    ndc_x = (2.0 * x) / screen_width - 1.0
-    ndc_y = 1.0 - (2.0 * y) / screen_height
-
-    clip_coords = np.array([ndc_x, ndc_y, -1.0, 1.0])
-    inv_projection_matrix = np.linalg.inv(np.array(projection_matrix).reshape(4,4))
-    inv_view_matrix = np.linalg.inv(np.array(view_matrix).reshape(4,4))
-    eye_coords = np.dot(inv_projection_matrix, clip_coords)
-    eye_coords = np.array([eye_coords[0], eye_coords[1], -1.0, 0.0])
-
-    world_coords = np.dot(inv_view_matrix, eye_coords)[:3]
-    camera_position = p.getDebugVisualizerCamera()[11]
-    ray_from = np.array(camera_position)
-    ray_to = ray_from + world_coords
-    return ray_from, ray_to
-
-initial_guess = [0,0,0,0]
-bounds = [(-150,150)]*num_joints
+    # 距離が一定以下ならオブジェクトを掴む
+    if distance < 0.1:
+        p.createConstraint(
+            parentBodyUniqueId=robot_id,
+            parentLinkIndex=end_effector_link_index,
+            childBodyUniqueId=object_id,
+            childLinkIndex=-1,
+            jointType=p.JOINT_FIXED,
+            jointAxis=[0, 0, 0],
+            parentFramePosition=[0, 0, 0],
+            childFramePosition=[0, 0, 0]
+        )
+        print("Object grasped!")
 
 while True:
-    mouse_events = p.getMouseEvents()
-    for event in mouse_events:
-        if event[0] & 0x01:
-            print("clicked")
-            x, y = event[1], event[2]
-            view_matrix = p.computeViewMatrixFromYawPitchRoll([0,0,0],1.0,90,-30,0,2)
-            projection_matrix = p.computeProjectionMatrixFOV(60,1,0.1,100)
-            ray_from, ray_to = get_ray_from_screen(x, y, view_matrix, projection_matrix)
+    keys = p.getKeyboardEvents()
+    for key, state in keys.items():
+        if state & p.KEY_WAS_TRIGGERED or state & p.KEY_IS_DOWN:
+            update_joint_angles(key)
+            if key == p.B3G_RETURN:
+                grasp_object()
 
-            # ray_toはカメラ方向に伸びた点であり、そのままでは地面などに当たらないことが多い
-            # 必要に応じてレイキャストして、地面やオブジェクトとの交点を取得してもよい。
-            # ここでは簡易的にray_to自体を目標位置とする。
-            target_position = ray_to
-
-            result = minimize(objective, initial_guess, args=(target_position, joint_lengths), bounds=bounds)
-            optimal_angles = result.x
-            initial_guess = optimal_angles
-
-            for i in range(num_joints):
-                p.setJointMotorControl2(robot_id, i, p.POSITION_CONTROL, np.radians(optimal_angles[i]))
+    # 計算された関節角度を適用
+    for i in range(num_joints):
+        p.setJointMotorControl2(robot_id, i, p.POSITION_CONTROL, joint_angles[i])
 
     p.stepSimulation()
     time.sleep(1./240.)
--- sim_test/sim3.py
+++ sim_test/sim3.py
@@ -1,82 +1,58 @@
+#オブジェクトの位置を指定して、逆運動学を解いて，アームを動かす
 import pybullet as p
 import time
 import pybullet_data
 import numpy as np
 
-# PyBulletのGUIモードで接続
 p.connect(p.GUI)
 p.setGravity(0, 0, -9.81)
 p.setAdditionalSearchPath(pybullet_data.getDataPath())
 
-# ロボットのURDFファイルをロード
-robot_id = p.loadURDF("../al5d_description/urdf/al5d_robot.urdf", basePosition=[0,0,0.1], useFixedBase=True)
+robot_orientation = p.getQuaternionFromEuler([0, 0, np.pi])
+robot_id = p.loadURDF("../al5d_description/urdf/al5d_robot.urdf", basePosition=[0.6, 0, 0.6], baseOrientation=robot_orientation, useFixedBase=True, globalScaling=2.0)
 
-# 平面追加（クリック時の衝突点を取得するため）
+# 平面追加
 plane_id = p.loadURDF("plane.urdf")
+table_id = p.loadURDF("table/table.urdf", basePosition=[0.5, 0, 0])
+
+# オブジェクトを追加
+object1_id = p.loadURDF("random_urdfs/000/000.urdf", basePosition=[0.2, -0.2, 0.6], globalScaling=0.8)
+object2_id = p.loadURDF("random_urdfs/000/000.urdf", basePosition=[0.1, 0.3, 0.6], globalScaling=0.8)
 
 num_joints = p.getNumJoints(robot_id)
-end_effector_link_index = num_joints - 1  # 末端リンクをエンドエフェクタとする
+end_effector_link_index = num_joints - 1
 
-# カメラ位置を調整
-p.resetDebugVisualizerCamera(cameraDistance=0.5, cameraYaw=90, cameraPitch=-30, cameraTargetPosition=[0,0,0.1])
+p.resetDebugVisualizerCamera(cameraDistance=1.0, cameraYaw=90, cameraPitch=-40, cameraTargetPosition=[0.1,0,1.5])
 
-def get_ray_from_screen(x, y, view_matrix, projection_matrix):
-    screen_width = 1024
-    screen_height = 768
-    ndc_x = (2.0 * x) / screen_width - 1.0
-    ndc_y = 1.0 - (2.0 * y) / screen_height
+def move_to_target(target_position):
+    # 逆運動学を解いて関節角度を計算
+    joint_angles = p.calculateInverseKinematics(robot_id, end_effector_link_index, target_position)
+    
+    # 計算された関節角度を適用
+    for i in range(num_joints):
+        p.setJointMotorControl2(robot_id, i, p.POSITION_CONTROL, joint_angles[i])
 
-    clip_coords = np.array([ndc_x, ndc_y, -1.0, 1.0])
-    inv_projection_matrix = np.linalg.inv(np.array(projection_matrix).reshape(4,4))
-    inv_view_matrix = np.linalg.inv(np.array(view_matrix).reshape(4,4))
-    eye_coords = np.dot(inv_projection_matrix, clip_coords)
-    eye_coords = np.array([eye_coords[0], eye_coords[1], -1.0, 0.0])
+def reset_to_initial_position():
+    # 初期姿勢に戻すための関節角度を設定
+    initial_joint_angles = [0] * num_joints
+    initial_joint_angles[2] -= 1.5
+    
+    for i in range(num_joints):
+        p.setJointMotorControl2(robot_id, i, p.POSITION_CONTROL, initial_joint_angles[i])
 
-    world_coords = np.dot(inv_view_matrix, eye_coords)[:3]
-    camera_position = p.getDebugVisualizerCamera()[11]
-    ray_from = np.array(camera_position)
-    ray_to = ray_from + world_coords
-    return ray_from, ray_to
 
 while True:
-    mouse_events = p.getMouseEvents()
-    for event in mouse_events:
-        if event[0] & 0x01:
-            print("clicked")
-            x, y = event[1], event[2]
-
-            # 現在のカメラ情報取得
-            cam_info = p.getDebugVisualizerCamera()
-            view_matrix = cam_info[2]
-            projection_matrix = cam_info[3]
-
-            # クリック位置からレイ生成
-            ray_from, ray_to = get_ray_from_screen(x, y, view_matrix, projection_matrix)
-            
-            # レイキャストして平面との交点を取得
-            # p.rayTestは ray_from から ray_to までの直線上で衝突検出を行う
-            # ray_toを遠くに延ばすことで地面との交点を確実に探す
-            extended_ray_to = ray_from + (ray_to - ray_from) * 10.0
-            hits = p.rayTest(ray_from, extended_ray_to)
-            
-            # hitsは[(hitObjectUniqueId, hitLinkIndex, hitFraction, hitPosition, hitNormal), ...]を返す
-            # 地面と衝突した場合、hitPositionで衝突点が得られる
-            if hits and hits[0][0] != -1:
-                hit_pos = hits[0][3]
-                target_position = hit_pos
-
-                # IK計算
-                # 関節角はp.calculateInverseKinematicsで直接求められる
-                joint_angles = p.calculateInverseKinematics(
-                    robot_id,
-                    end_effector_link_index,
-                    target_position,
-                    # 姿勢指定しない場合はデフォルト姿勢
-                )
-
-                # 計算された関節角度を適用
-                for i in range(num_joints):
-                    p.setJointMotorControl2(robot_id, i, p.POSITION_CONTROL, joint_angles[i])
+    keys = p.getKeyboardEvents()
+    for key, state in keys.items():
+        if state & p.KEY_WAS_TRIGGERED or state & p.KEY_IS_DOWN:
+            if key == ord('1'):
+                object1_pos, _ = p.getBasePositionAndOrientation(object1_id)
+                move_to_target(object1_pos)
+            elif key == ord('2'):
+                object2_pos, _ = p.getBasePositionAndOrientation(object2_id)
+                move_to_target(object2_pos)
+            elif key == ord('0'):  #初期姿勢に戻る
+                reset_to_initial_position()
 
     p.stepSimulation()
     time.sleep(1./240.)
